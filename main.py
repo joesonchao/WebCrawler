0837fc7368642ca0473c2496102665d54f5c163d
Joeson <joesonchao@gmail.com>
2021-01-29 10:40:42 +0800

    相簿爬蟲

--- main.py
+++ main.py
@@ -2,23 +2,19 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 import os
-import requests as req
 from tqdm import tqdm
-
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
 
 FQDN = "https://photo.xuite.net/"
-# albumName = "ntuhwatermelon"
+# 輸入你想要的相簿名稱
+albumName = "ntuhwatermelon"
 # albumName = "tsaibill5838"
-albumPages = 1
-DownloadPath = "D:\\"
+
+albumPages = 1  # 預設相簿頁數
+DownloadPath = "D:\\"  # 預設相簿路徑
 LocalFolder = ""
 
 
@@ -29,36 +25,65 @@ def parse_page(urlpage):
     return soup
 
 
+# 下載時顯示進度條
+def download(url):
+    filename = url.split('/')[-1]
+    parseResult = urlparse(url)
+    savefile = LocalFolder + "\\" + filename
+
+    # print(savefile)
+
+    if CheckFile(savefile) == False:
+        try:
+            img = requests.get(url)
+            content_size = int(img.headers['Content-Length']) / 1024
+            with open(savefile, "wb") as file:  # 開啟資料夾及命名圖片檔
+                for data in tqdm(iterable=img.iter_content(1024), total=content_size, unit='k', desc=savefile):
+                    file.write(img.content)  # 寫入圖片的二進位碼
+                # print("Download File:" + savefile + " Complete")
+        except:
+            print("Download Fail:" + savefile)
+    else:
+        print(savefile + " File already exist.")
+
+
 # 取得相簿分頁
 def get_album(albumName):
-    global albumPages, LocalFolder
+    global albumPages, LocalFolder  # access global variable
     if not albumName:
         print("未輸入相簿名稱")
         return
     url = FQDN + albumName
 
-    albumPages = get_MaxPage(url)  # 取得相簿最大頁面
-    print("相簿" + albumName + "共" + albumPages + "頁")
+    albumPages = get_MaxPage(url)  # 取得共有幾本相簿
+    print("相簿" + albumName + "共" + albumPages + "頁相本")
     # 檢查目錄是否存在
     LocalFolder = DownloadPath + albumName
     CheckFolder(LocalFolder)
 
     parseResult = urlparse(url)
-    # print("parseResult:" + parseResult.geturl())
+
+    # 進行每一頁相簿擷取
     for single in range(int(albumPages)):
         page = (parseResult.geturl() + "*" + str(single + 1))
-
         Get_albumPage(page)
-        # parse_page(page)
+
+
+def CheckFile(LocalFile):
+    # 檢查檔案是否存在
+    if os.path.isfile(LocalFile):
+        return True
+    else:
+        return False
 
 
 # 檢查目錄是否存在，若不存在則建立
 def CheckFolder(LocalFolder):
     if os.path.isdir(LocalFolder):
-        print("目錄" + LocalFolder + "已存在")
+        # print("目錄" + LocalFolder + "已存在")
         pass
     else:
-        print("產生" + LocalFolder + "目錄")
+        # print("產生" + LocalFolder + "目錄")
         os.makedirs(LocalFolder)
 
 
@@ -67,24 +92,21 @@ def Get_albumPage(url):
     parseResult = urlparse(url)
     soup = parse_page(url)
     sel = soup.select("p.album_info_title > a")
-
+    # count = 0
     for single in sel:
         linkpage = parseResult.scheme + ':' + single["href"]
-        # print("===Start Getting " + single.text +"==")
-        pages = str(get_MaxPage(linkpage))
-        print("「" + single.text + " " + parseResult.scheme + ':' + single["href"] + "」共" + pages + "頁")
-        # print("===End of Getting " + single.text +"==")
+        # if count == 0:
+        Get_Photo(linkpage)
+        # count += 1
 
 
-# 取得相片
-def Get_Photo(url):
-    global LocalFolder
-    soup = parse_page(url)
-    password = "23710148"
-    checkpwd = soup.find(id="pwd")
+# 判斷相簿頁面是否需要輸入密碼
+def get_EncryptURL(url):
+    content = parse_page(url)
+    password = "23710148"  # 相簿密碼，目前版本只支援全部相簿密碼都一樣
+    checkpwd = content.find(id="pwd")
     if checkpwd is not None:
-        print("請輸入密碼")
-
+        #####################################################
         # 這一段是從POSTMAN產出的
         payload = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"pwd\"\r\n\r\n" + password + "\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--"
         headers = {
@@ -92,44 +114,37 @@ def Get_Photo(url):
             'cache-control': "no-cache",
             'postman-token': "084ee711-0f42-de63-4507-d1eed28dbce9"
         }
-
-        response = requests.request("POST", url, data=payload, headers=headers)
+        #####################################################
+        response = requests.request("POST", url, data=payload, headers=headers)  # 取得輸入密碼後的頁面內容
         soup = BeautifulSoup(response.text, "html.parser")  # 將網頁資料以html.parser
-        # print(soup)
-    else:
-        print("不需要輸入密碼")
-        # return soup
+        return soup
+    else:  # 若不需要密碼直接回傳頁面內容
+        return content
 
-    # 取得頁數，但排除下一頁及最後一頁
-    pages = soup.select("div.page > a:not(#nav-next, #nav-last)")
 
-    titleSoup = soup.select("div.title > span.titlename > a:not(#nav-parent)")
-    # text = soup.find("div.title > span.titlename > a")
+# 取得相片
+def Get_Photo(url):
+    global LocalFolder
+    pages = str(get_MaxPage(url))  # 取得相簿分頁數
+
+    content = get_EncryptURL(url)
+
     # 取得相簿名稱
-    if len(titleSoup) == 1:
+    titleSoup = content.select("div.title > span.titlename > a:not(#nav-parent)")
+    if titleSoup is not None:
         AlbumTitle = titleSoup[0].text
+        LocalFolder = DownloadPath + albumName + "\\" + AlbumTitle
+        CheckFolder(LocalFolder)
+    else:
+        print("titleSoup is None")
 
-    LocalFolder = DownloadPath + albumName + "\\" + AlbumTitle
-    CheckFolder(LocalFolder)
-
-    # Region 取得相簿中相片頁數
-    print(len(pages) + 1)
-    for page in pages:
-        # print(page["href"])
-        response = requests.get(page["href"])  # 將網頁資料GET下來
-        sel = soup.select("div.photo_item.inline-block > a > img")
-        count = 0
+    # print("Pages: " + pages)
+    for page in range(int(pages)):
+        # response = requests.get(page["href"])  # 將網頁資料GET下來
+        sel = content.select("div.photo_item.inline-block > a > img")
 
         for showdiv in sel:
-            print(showdiv["alt"])
-
-            print(showdiv["src"].replace('_c', '_x'))
-            # response = requests.get(showdiv["src"].replace('_c', '_x'))
-            if count == 0:
-                print(showdiv["src"].replace('_c', '_x'))
-                #download(showdiv["src"].replace('_c', '_x'))
-
-            count += 1
+            download(showdiv["src"].replace('_c', '_x'))
 
 
 # 取得相簿帳號最大頁數
@@ -137,7 +152,7 @@ def get_MaxPage(url):
     MaxPage = 1
     page_content = parse_page(url)
     sel = page_content.select("div.page > #nav-last")
-    print(len(sel))
+    # print(len(sel))
 
     if (len(sel) == 1):
         print(sel[0]["href"])
@@ -147,22 +162,7 @@ def get_MaxPage(url):
             MaxPage = SplitArr[1]
     return MaxPage
 
-# 下載時顯示進度條
-def download(url):
-    #filename = url.split('/')[-1]
-    parseResult = urlparse(url)
-    savefile = LocalFolder + "\\" + parseResult.path
-    r = req.get(url, stream=True)
-    with open(savefile, 'wb') as f:
-        for data in tqdm(r.iter_content(1024)):
-            f.write(data)
-    return savefile
 
-# Press the green button in the gutter to run the script.
 # Main Function
 if __name__ == '__main__':
-    albumName = "ntuhwatermelon"
-    # get_album(albumName)
-    page_inside = "https://photo.xuite.net/ntuhwatermelon/20546016"  # Need password
-    Get_Photo(page_inside)
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    get_album(albumName)
